9af87e7c61c54b479df77cd22c3771c9bd9cfbbf
alex <a.loretzin@gmail.com>
2018-08-20 15:12:41 +0200

    update produit.php

--- include/produits.php
+++ include/produits.php
@@ -18,12 +18,19 @@ if(!$produits) {
 
 //affichage des produits
 foreach ($produits->fetchAll() as $produits){ ?>
-<div>
-     <?php echo $produits['image_produit']; ?>
+<div class="produit-container row">
+    <div class="image-produit col-2">
+        <img src="<?php echo $produits['image_produit'];?>">
+    </div>
+    <div class="texte-produit col-8 ">
+        <div class="row">
+            <h2> <?php echo htmlspecialchars($produits['nom_produit']); ?> </h2>
+            <span class="label label-default"> <?php echo htmlspecialchars($produits['categorie_produit']); ?></span>
+        </div>
+    </div>
+    <div class="col-2" >
 
-    <h2> <?php echo htmlspecialchars($produits['nom_produit']); ?> </h2>
-    <h4> <?php echo htmlspecialchars($produits['categorie_produit']); ?></h4>
-    <p> <?php echo $produits['description_produit']; ?> </p>
+    </div>
 </div>
 <?php } ?>
 
