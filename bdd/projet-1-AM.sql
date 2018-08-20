62374b6a25aa1c3b164a6cc37cb53e6220e01273
alex <a.loretzin@gmail.com>
2018-08-20 13:58:34 +0200

    ajout php

--- bdd/projet-1-AM.sql
+++ bdd/projet-1-AM.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Aug 20, 2018 at 10:13 AM
+-- Generation Time: Aug 20, 2018 at 01:57 PM
 -- Server version: 10.1.34-MariaDB
 -- PHP Version: 7.2.7
 
@@ -35,6 +35,20 @@ CREATE TABLE `produits` (
   `description_produit` text NOT NULL,
   `image_produit` varchar(1000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `produits`
+--
+
+INSERT INTO `produits` (`id`, `nom_produit`, `categorie_produit`, `description_produit`, `image_produit`) VALUES
+(1, 'Objet bidon 1', 'Bidon', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dignissim elit magna, in commodo nibh bibendum et. Pellentesque scelerisque nisl metus, id ullamcorper sapien suscipit at. Nulla facilisi. In hac habitasse platea dictumst. Quisque consequat ligula id ipsum dignissim, et.', 'image'),
+(2, 'Objet bidon 2', 'Bidon', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dignissim elit magna, in commodo nibh bibendum et. Pellentesque scelerisque nisl metus, id ullamcorper sapien suscipit at. Nulla facilisi. In hac habitasse platea dictumst. Quisque consequat ligula id ipsum dignissim, et.', 'image'),
+(3, 'Objet bidon 3', 'Bidon', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dignissim elit magna, in commodo nibh bibendum et. Pellentesque scelerisque nisl metus, id ullamcorper sapien suscipit at. Nulla facilisi. In hac habitasse platea dictumst. Quisque consequat ligula id ipsum dignissim, et.', 'image'),
+(4, 'Objet bidon 4', 'Bidon', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dignissim elit magna, in commodo nibh bibendum et. Pellentesque scelerisque nisl metus, id ullamcorper sapien suscipit at. Nulla facilisi. In hac habitasse platea dictumst. Quisque consequat ligula id ipsum dignissim, et.', 'image'),
+(5, 'Objet machin 1', 'Machin', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dignissim elit magna, in commodo nibh bibendum et. Pellentesque scelerisque nisl metus, id ullamcorper sapien suscipit at. Nulla facilisi. In hac habitasse platea dictumst. Quisque consequat ligula id ipsum dignissim, et.', 'image'),
+(6, 'Objet machin 2', 'Machin', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dignissim elit magna, in commodo nibh bibendum et. Pellentesque scelerisque nisl metus, id ullamcorper sapien suscipit at. Nulla facilisi. In hac habitasse platea dictumst. Quisque consequat ligula id ipsum dignissim, et.', 'image'),
+(7, 'Objet machin 3', 'Machin', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dignissim elit magna, in commodo nibh bibendum et. Pellentesque scelerisque nisl metus, id ullamcorper sapien suscipit at. Nulla facilisi. In hac habitasse platea dictumst. Quisque consequat ligula id ipsum dignissim, et.', 'image'),
+(8, 'Objet machin 4', 'Machin', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dignissim elit magna, in commodo nibh bibendum et. Pellentesque scelerisque nisl metus, id ullamcorper sapien suscipit at. Nulla facilisi. In hac habitasse platea dictumst. Quisque consequat ligula id ipsum dignissim, et.', 'image');
 
 -- --------------------------------------------------------
 
@@ -71,7 +85,7 @@ ALTER TABLE `vote`
 -- AUTO_INCREMENT for table `produits`
 --
 ALTER TABLE `produits`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
